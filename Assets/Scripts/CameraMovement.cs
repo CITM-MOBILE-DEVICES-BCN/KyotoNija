a10c9b174e5d43236fdd5edb3bf3b31e12ed920c
VM Marco <marcoxativa@gmail.com>
2024-12-03 07:13:15 +0100

    camera

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+
+    public PlayerMovement player;
+
+    public const int distanceParam = 8;
+
+    Vector2 normalSpeed = new Vector2(0, 8);
+    Vector2 closeSpeed  = new Vector2(0, 20);
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(transform.position.y - player.transform.position.y) > distanceParam)
+        {
+            transform.position += (Vector3)normalSpeed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += (Vector3)closeSpeed * Time.deltaTime;
+        }
+    }
+}
